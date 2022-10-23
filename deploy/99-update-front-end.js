12dfb993d8b50427029959b7f284e14b815b2965
nachoddiaz <nachodiaz00@hotmail.es>
2022-10-23 21:57:16 +0200

    update

---
+++ deploy/99-update-front-end.js
@@ -0,0 +1,37 @@
+//Queremos que al cambiar el backend, el frontend le siga
+
+
+const fs = require("fs")
+const { network } = require("hardhat")
+
+const frontEndContractsFile = "../nextjs-nft-marketplace-moralis/constants/networkMapping.json"
+const frontEndAbiFile = "../nextjs-nft-marketplace-moralis/constants/abi.json"
+
+module.exports = async () => {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("Updating to front end...")
+        await updateContractAddresses()
+        await updateAbi()
+        console.log("Front end written!")
+    }
+}
+
+async function updateAbi() {
+    const NftMarketplace = await ethers.getContract("NftMarketplace")
+    fs.writeFileSync(frontEndAbiFile, NftMarketplace.interface.format(ethers.utils.FormatTypes.json))
+}
+
+async function updateContractAddresses() {
+    const NftMarketplace = await ethers.getContract("NftMarketplace")
+    const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
+    if (network.config.chainId.toString() in contractAddresses) {
+        if (!contractAddresses[network.config.chainId.toString()].includes(NftMarketplace.address)) {
+            contractAddresses[network.config.chainId.toString()].push(NftMarketplace.address)
+        }
+    } else {
+        contractAddresses[network.config.chainId.toString()] = [NftMarketplace.address]
+    }
+    fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
+}
+module.exports.tags = ["all", "frontend"]
+
