1f23e238dc976a89ebc104ab34362d9e14b6f408
nachoddiaz <nachodiazutr@gmail.com>
2022-10-25 22:35:31 +0200

    buy item script added

---
+++ scripts/buy-item.js
@@ -0,0 +1,24 @@
+const { ethers } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
+
+TOKEN_ID = 2
+
+async function buy() {
+    const NftMarketplace = await ethers.getContract("NftMarketplace")
+    const BasicNFT = await ethers.getContract("BasicNFT")
+    const listing = await NftMarketplace.getListings(BasicNFT.address, TOKEN_ID)
+    const price = listing.price.toString()
+    const tx = await NftMarketplace.buyItem(BasicNFT.address, TOKEN_ID, { value : price })
+    await tx.wait(1)
+    console.log("NFT Bought!!")
+    if (network.config.chainId == 31337) {
+        await moveBlocks(2, (sleepAmount = 1000))
+    }
+}
+
+buy()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
